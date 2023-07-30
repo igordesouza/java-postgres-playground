5aef87e823fe147fcc8cab67328ea97e105cb3ed
Igor de Souza <igordesouza182@gmail.com>
2023-07-30 22:57:27 +0000

    Aula 4. Tipos char, boolean e a palavra reservada var do Java 10

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -10,10 +10,21 @@ public class App {
         // cast. conversão de tipo. pode estourar.
         // sem cast o código não roda.
         // error: Type mismatch: cannot convert from int to byte
-        int idadeInt = 129;
-        byte idade = (byte)idadeInt; 
-        double peso = 70;
+        // int idadeInt = 129;
+        // byte idade = (byte)idadeInt; 
+        // double peso = 70;
+        byte idade = 42;
+        double peso = 85;
 
         System.out.println("Idade é " + idade + " e peso é " + peso);
+
+        char sexo = 'M';
+
+        System.out.println("Sexo: " + sexo);
+
+        // o tipo pode ser var, mas um valor pode ser definido
+        boolean necessidadesEspeciais = true;
+        System.out.println("Tem necessidades especiais:" + necessidadesEspeciais);
+
     }
 }
